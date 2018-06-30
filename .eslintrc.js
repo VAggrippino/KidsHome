322e7c34ae9cd73ec51a9bc86196265bb8f5b24a
Vince Aggrippino <vinceaggrippino@gmail.com>
2018-06-30 09:25:08 +0800

    Initialize npm and install ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true
+    },
+    "rules": {
+        "no-multiple-empty-lines": ["error", {"max": 1, "maxEOF": 0 }],
+        "no-use-before-define": ["off"]
+    }
+};
